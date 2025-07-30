46882b980ffd861314bfc25bbac1b41e25ab51f6
Zsolt Harmath <harmathzs@gmail.com>
2025-07-30 22:58:41 +0200

    Add TabNav

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,23 +1,30 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Routes, Route, Link } from "react-router-dom";
+import { TabNav } from "@radix-ui/themes";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 import Home from './Home';
 import UiElements from './UiElements';
 import MyForm from './MyForm';
 
 function App() {
+  const location = useLocation();   // get location object
+  const pathname = location.pathname;  // current URL path
+
   return (
     <>
       <div>
         {/* Navigation */}
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/uielements">UiElements</Link></li>
-            <li><Link to="/myform">MyForm</Link></li>
-          </ul>
-        </nav>
+        <TabNav.Root>
+          <TabNav.Link asChild active={pathname === "/"}>
+            <Link to="/">Home</Link>
+          </TabNav.Link>
+          <TabNav.Link asChild active={pathname === "/uielements"}>
+            <Link to="/uielements">UI Elements</Link>
+          </TabNav.Link>
+          <TabNav.Link asChild active={pathname === "/myform"}>
+            <Link to="/myform">My Form</Link>
+          </TabNav.Link>
+        </TabNav.Root>
+
         {/* Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
