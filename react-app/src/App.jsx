89f31dcfba3541a0f70be92caf82d214d0836a3d
Zsolt Harmath <harmathzs@gmail.com>
2025-07-30 14:11:37 +0200

    Specify routes

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Home';
+import UiElements from './UiElements';
+import MyForm from './MyForm';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +32,22 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        {/* Navigation */}
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/uielements">UiElements</Link></li>
+            <li><Link to="/myform">MyForm</Link></li>
+          </ul>
+        </nav>
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/uielements" element={<UiElements />} />
+          <Route path="/myform" element={<MyForm />} />
+        </Routes>
+      </div>
     </>
   )
 }
---
+++ react-app/src/Home.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+export default class Home extends React.Component {
+    render() {
+        return <h2>Home</h2>;
+    }
+}
