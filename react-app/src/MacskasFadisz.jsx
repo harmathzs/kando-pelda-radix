cbd93b7cdff5d87341bfb42eaec7f322741837ae
Zsolt Harmath <harmathzs@gmail.com>
2025-07-31 12:23:41 +0200

    Add props

--- react-app/src/MacskasFadisz.jsx
+++ react-app/src/MacskasFadisz.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { Box, Container } from "@radix-ui/themes";
 export default class MacskasFadisz extends React.Component {
+    constructor({text}) {
+        super({text});
+
+        this.state = {
+            text: text
+        }
+    }
+
     render() {
-        return <p>Macskás Fadísz</p>
+        return <div>
+            <Box>
+                <p>{this.state.text}</p>
+            </Box>
+        </div>;
     }
 }
