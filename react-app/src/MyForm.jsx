ce2972c59365d1d8c4a9560a864553c11ffabf9a
Zsolt Harmath <harmathzs@gmail.com>
2025-08-09 15:15:49 +0200

    Add form elements

--- react-app/src/MyForm.jsx
+++ react-app/src/MyForm.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Flex, Grid, Box, Dialog, Button, Card, Text, TextField, Checkbox, TextArea, Slider } from "@radix-ui/themes";
+import { Flex, Box, Dialog, Button, Checkbox, Text, TextField, CheckboxCards, RadioCards, Select, TextArea, Slider, Switch } from "@radix-ui/themes";
 import { FilePlusIcon } from "@radix-ui/react-icons";
 
 export default class MyForm extends React.Component {
+    handleFormSubmit = e => {
+        console.log('handleFormSubmit e', e);
+    }
+
     render() {
         return <Box style={{padding: '15px'}}>
                     <Dialog.Root>
@@ -16,13 +20,14 @@ export default class MyForm extends React.Component {
                                 Register new user.
                             </Dialog.Description>
 
-                            <form>
+                            <form method='POST'>
                                 <Flex direction="column" gap="3">
                                     <label>
                                         <Text as="div" size="2" mb="1" weight="bold">
                                             Name
                                         </Text>
                                         <TextField.Root
+                                            name='inputName'
                                             defaultValue="Freja Johnsen"
                                             placeholder="Enter your full name"
                                         />
@@ -32,11 +37,86 @@ export default class MyForm extends React.Component {
                                             Email
                                         </Text>
                                         <TextField.Root
+                                            name='inputEmail'
                                             defaultValue="freja@example.com"
                                             placeholder="Enter your email"
                                         />
                                     </label>
                                 </Flex>
+
+                                <Text as="label" size="2">
+                                    <Flex as="span" gap="2" justify="end">
+                                        <Checkbox name='checkboxMale' />
+                                        Male
+                                    </Flex>
+                                </Text>
+
+                                <Flex align="center" gap="3">
+                                    <RadioCards.Root defaultValue="1" columns={{ initial: "1", sm: "3" }}>
+                                        <RadioCards.Item value="1">
+                                            <Flex direction="column" width="100%">
+                                                <Text weight="bold">Free</Text>
+                                                <Text>Free user</Text>
+                                            </Flex>
+                                        </RadioCards.Item>
+                                        <RadioCards.Item value="2">
+                                            <Flex direction="column" width="100%">
+                                                <Text weight="bold">Cheap</Text>
+                                                <Text>Cheap package</Text>
+                                            </Flex>
+                                        </RadioCards.Item>
+                                        <RadioCards.Item value="3">
+                                            <Flex direction="column" width="100%">
+                                                <Text weight="bold">Expensive</Text>
+                                                <Text>Expensive package</Text>
+                                            </Flex>
+                                        </RadioCards.Item>
+                                    </RadioCards.Root>
+                                </Flex>
+
+                                <Flex gap="3" justify="end">
+                                    <Text as='label'>Role: </Text>
+                                    <Select.Root defaultValue="dev">
+                                        <Select.Trigger radius="full" />
+                                        <Select.Content>
+                                            <Select.Item value="dev">Developer</Select.Item>
+                                            <Select.Item value="salesrep">Sales rep</Select.Item>
+                                            <Select.Item value="am">Account manager</Select.Item>
+                                        </Select.Content>
+                                    </Select.Root>
+                                </Flex>
+
+                                <Flex direction="column" gap="4" maxWidth="300px">
+                                    <Text as='label'>English proficiency: </Text>
+                                    <Slider defaultValue={[25]} size="1" name='sliderEnglish' />
+                                    <Text as='label'>Salary expectation range: </Text>
+                                    <Slider min={0} max={12000} defaultValue={[1200, 4800]} size="1" name='sliderSalaryRange' />
+                                </Flex>
+
+                                <Flex gap="3" justify="end">
+                                    <Text as='label'>Like jokes</Text>
+                                    <Switch color="orange" name='switchLikeJokes' defaultChecked />
+                                </Flex>
+
+                                <Flex gap="3">
+                                    <TextArea style={{width: '600px'}} resize='both' name='textareaComments' placeholder='More comments...' />
+                                </Flex>
+
+                                <Flex gap="3">
+                                    <Box>
+                                        <p>&nbsp;</p>
+                                    </Box>
+                                </Flex>
+
+                                <Flex align="center" gap="3">
+                                    <CheckboxCards.Root defaultValue={["0"]} size="1">
+                                        <CheckboxCards.Item value="1">Agree to Terms</CheckboxCards.Item>
+                                    </CheckboxCards.Root>
+
+                                    <CheckboxCards.Root defaultValue={["0"]} size="1">
+                                        <CheckboxCards.Item value="1">Agree to Conditions</CheckboxCards.Item>
+                                    </CheckboxCards.Root>
+                                </Flex>                                
 
                                 <Flex gap="3" mt="4" justify="end">
                                     <Dialog.Close>
@@ -45,7 +125,7 @@ export default class MyForm extends React.Component {
                                         </Button>
                                     </Dialog.Close>
                                     <Dialog.Close>
-                                        <Button>Save</Button>
+                                        <Button onClick={this.handleFormSubmit}>Submit</Button>
                                     </Dialog.Close>
                                 </Flex>
                             </form>
