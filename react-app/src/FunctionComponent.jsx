9ee8f3b352e684a3da72025fab4f5f795f72a33f
Zsolt Harmath <harmathzs@gmail.com>
2025-08-10 14:12:31 +0200

    Mimic state in function component

--- react-app/src/FunctionComponent.jsx
+++ react-app/src/FunctionComponent.jsx
@@ -1,4 +1,27 @@
+import { useState } from 'react';
+
 const FunctionComponent = (props) => {
-    return <p>FunctionComponent</p>
+    const [numbers, setNumbers] = useState([1, 4, 2, 8]);
+
+    const handleAppend = e => {
+        console.log('handleAppend e', e);
+
+        const rnd = Math.floor(Math.random()*9+1);
+        const newNumbers = [...this.state.numbers];
+        newNumbers.push(rnd);
+        setNumbers({numbers: [...newNumbers]});
+    }
+
+    return <div>
+            <p>
+                <button onClick={handleAppend}>Append a new number</button>
+            </p>
+            <p>
+                {numbers.map((num, i)=><span key={i}>{num}, </span>)}
+            </p>
+            <p>
+                Props: {JSON.stringify(props)}
+            </p>
+        </div>
 }
 export default FunctionComponent;
