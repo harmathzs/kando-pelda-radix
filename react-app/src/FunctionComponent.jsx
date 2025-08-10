26afd8a297bea13a03c43e9cbb82570306b815db
Zsolt Harmath <harmathzs@gmail.com>
2025-08-10 14:18:31 +0200

    Mimic lifecycle methods in function component

--- react-app/src/FunctionComponent.jsx
+++ react-app/src/FunctionComponent.jsx
@@ -1,27 +1,43 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const FunctionComponent = (props) => {
-    const [numbers, setNumbers] = useState([1, 4, 2, 8]);
+  const [numbers, setNumbers] = useState([1, 4, 2, 8]);
 
-    const handleAppend = e => {
-        console.log('handleAppend e', e);
+  const handleAppend = e => {
+    const rnd = Math.floor(Math.random() * 9 + 1);
+    // Correct usage: update array, since numbers is the actual array
+    setNumbers([...numbers, rnd]);
+  };
 
-        const rnd = Math.floor(Math.random()*9+1);
-        const newNumbers = [...this.state.numbers];
-        newNumbers.push(rnd);
-        setNumbers({numbers: [...newNumbers]});
+  // componentDidMount
+  useEffect(() => {
+    console.log('Mounted');
+    // componentWillUnmount
+    return () => {
+      console.log('UnMounted');
+    };
+  }, []);
+
+  // componentDidUpdate for numbers
+  useEffect(() => {
+    if (numbers.length > 4) { // Only log after first append, for demo
+      console.log('Numbers changed: ', numbers);
     }
+  }, [numbers]);
 
-    return <div>
-            <p>
-                <button onClick={handleAppend}>Append a new number</button>
-            </p>
-            <p>
-                {numbers.map((num, i)=><span key={i}>{num}, </span>)}
-            </p>
-            <p>
-                Props: {JSON.stringify(props)}
-            </p>
-        </div>
+  return (
+    <div>
+      <p>
+        <button onClick={handleAppend}>Append a new number</button>
+      </p>
+      <p>
+        {numbers.map((num, i) => <span key={i}>{num}, </span>)}
+      </p>
+      <p>
+        Props: {JSON.stringify(props)}
+      </p>
+    </div>
+  );
 }
+
 export default FunctionComponent;
