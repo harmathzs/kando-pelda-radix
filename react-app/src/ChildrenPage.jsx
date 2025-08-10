ec1ae221253a395450b39f5a33e8feff60d09dd1
Zsolt Harmath <harmathzs@gmail.com>
2025-08-10 14:50:30 +0200

    Add child dinamically

--- react-app/src/ChildrenPage.jsx
+++ react-app/src/ChildrenPage.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Parent from "./Parent";
 import Child from "./Child";
 export default class ChildrenPage extends React.Component {
+    state = {
+        cntChildren: 3,
+    }
+
     render() {
         return <div>
+            <p>
+                <button onClick={()=>this.setState({cntChildren: this.state.cntChildren+1})}>Add child</button>
+            </p>
             <Parent>
-                <Child name="John" />
-                <Child name="Dana" />
-                <Child name="Tim" />
+                {Array.from({ length: this.state.cntChildren }, (_, i) =>
+                    <Child key={i} name={'Tom'+i} />
+                )}
             </Parent>
         </div>
     }
